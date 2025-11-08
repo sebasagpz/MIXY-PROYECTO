69c80838316af9e873d5864164f427281978ef61
emartinezposso-crypto <edimartinezposso@gmail.com>
2025-11-08 16:21:11 -0500

    Terminado la conexion y la base de datos EN INDEX.HTML INCLUIDO JAVASCRIP}

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 from psycopg2 import connect, extras
 from cryptography.fernet import Fernet
 
@@ -18,9 +18,9 @@ key = Fernet.generate_key()
 #se crea las constante para conectarse a postgres
 host = 'localhost' 
 port = 5432
-dbname = 'PROYECTO_MIXY' #nombre de la base de datos
+dbname = 'proyecto_mixy' #nombre de la base de datos
 username = 'postgres'
-password = 123456
+password = '123456'
 
 """Se definen las constantes para conectarse a PostgreSQL:
 host: dirección del servidor.
@@ -29,6 +29,10 @@ dbname: nombre de la base de datos.
 username: usuario de PostgreSQL.
 password: contraseña del usuario."""
 
+@app.route('/')
+def inicio():
+    return send_file('templates/index.html')
+
 def get_connection(): 
     #propiedades
     conn = connect(host=host, port=port, dbname=dbname, user=username, password=password)
@@ -36,13 +40,13 @@ def get_connection():
     """Esta función crea y devuelve una conexión activa con la base de datos 
     PostgreSQL usando los parámetros definidos anteriormente."""
 
-@app.get('/api/users') #Ruta: /api/users  con metodo get
+@app.get('/datos') #Ruta: /api/users  con metodo get
 def get_users():
     conn = get_connection()
     cur = conn.cursor(cursor_factory=extras.RealDictCursor)
     
 
-    cur.execute('SELECT * FROM users')
+    cur.execute('SELECT * FROM usuarios')
     users = cur.fetchall()
 
     cur.close()
@@ -53,17 +57,22 @@ def get_users():
 Se ejecuta una consulta SQL que selecciona todos los usuarios.
 Se devuelven los resultados como una lista de diccionarios JSON."""
 
-@app.post('/api/users') #Ruta: /api/users
+@app.post('/dato/api' ) #Ruta: /api/users
 def create_user():
     new_user = request.get_json()
-    email = new_user['email']
+    nombre = new_user['nombre']
+    apellido = new_user['apellido']
+    correo = new_user['correo']
+    nacimiento = new_user['nacimiento']
     password = Fernet(key).encrypt(bytes(new_user['password'], 'utf-8'))
     
     conn = get_connection()
     cur = conn.cursor()
     
-    cur.execute('INSERT INTO users (email, password) VALUES (%s, %s) RETURNING *', 
-                (email, password))
+    cur.execute('INSERT INTO usuarios (nombre, apellido, correo, nacimiento, password) VALUES (%s, %s, %s, %s, %s) RETURNING *', 
+            (nombre, apellido, correo, nacimiento, password))
+
+
     new_created_user = cur.fetchone()
     print(new_created_user)
     conn.commit()
@@ -75,64 +84,7 @@ def create_user():
 Se ejecuta una sentencia SQL INSERT.
 Se devuelve el nuevo usuario creado."""
 
-@app.delete('/api/users/<id>')  #Ruta: /api/users/<id>
-def delete_user(id):
-    conn = get_connection()
-    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
 
-    cur.execute('DELETE FROM users WHERE id = %s RETURNING * ', (id))
-    user = cur.fetchone()
-    
-    conn.commit()
-
-    conn.close()
-    cur.close()
-
-    if user is None:
-        return jsonify({'message': 'User not found'}), 404
-    return jsonify(user)
-    """Se elimina el usuario con el id especificado.
-Si no existe, se devuelve un mensaje de error 404.
-Si existe, se devuelve el usuario eliminado."""
-
-# @app.put('/api/users/<id>')
-# def update_user(id):
-    
-#     conn = get_connection()
-#     cur = conn.cursor(cursor_factory=extras.RealDictCursor)
-
-#     new_user = request.get_json()
-#     username = new_user['username']
-#     email = new_user['email']
-#     password = Fernet(key).encrypt(bytes(new_user['password'], 'utf-8'))
-
-#     cur.execute('UPDATE users SET username = %s, email = %s, password = %s WHERE id = %s RETURNIG *', (username, email, password, id))
-#     updated_user = cur.fetchone()
-
-#     conn.commit()
-
-#     cur.close()
-#     conn.close()
-
-
-#     if updated_user is None:
-#         return jsonify({'menssage': 'User not found'}), 404
-    
-#     return jsonify(updated_user)
-
-@app.get('/api/users/<id>')
-def get_user(id):
-    
-    conn = get_connection()
-    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
-
-    cur.execute('SELECT * FROM users WHERE id = %s', (id,))
-    user = cur.fetchone()
-    
-    if user is None:
-        return jsonify({'message': 'User not found'}), 404
-
-    return jsonify(user)
 
 if __name__ == '__main__':
     app.run(debug=True)
